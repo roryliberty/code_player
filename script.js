775ef091d3388a39ade7a391a6adc2084c0aad08
Rory Liberty <roryliberty@gmail.com>
2022-10-11 14:39:17 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#html-panel').resizeable({
+        containment: '#html-container'
+    });
+})
+
+// if (typeof jQuery != 'undefined') {
+//     // jQuery is loaded => print the version
+//     alert(jQuery.fn.jquery);
+// } else {
+//     alert('jquery is broke as fuck');
+// }
