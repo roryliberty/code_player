c4cb498bdcc227ca9ed52076d9f0cad3d119936b
Rory Liberty <roryliberty@gmail.com>
2022-10-12 10:59:44 -0500

    Saving

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+// Resizable html-panel
 $(function() {
-    $('#html-panel').resizeable({
-        containment: '#html-container'
-    });
+    $('#html-panel').resizable();
+});
+
+// Toggles colors on buttons when hovering
+$('.toggle-button').hover(function() {
+    $(this).addClass('highlighted-button');
+}, function () {
+    $(this).removeClass('highlighted-button');
+});
+
+// When buttons are clicked, add/remove active class
+$('.toggle-button').click(function() {
+    $(this).toggleClass('active');
 })
 
-// if (typeof jQuery != 'undefined') {
-//     // jQuery is loaded => print the version
-//     alert(jQuery.fn.jquery);
-// } else {
-//     alert('jquery is broke as fuck');
-// }
+// Sets the height of the panels
+$('#html-panel').height($('window').height() - $('#navbar').height());
+
+// Capture changes in html-panel
+$('textarea').on('change keyup paste', function() {
+    // Change html of output-panel iframe
+    $("#output-panel").contents().find("html").html($('#html-panel').val());
+});
