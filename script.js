9d8d5cb663cf367d9bb3a99dcdf159d542f314a3
Rory Liberty <roryliberty@gmail.com>
2022-10-12 14:35:20 -0500

    Done

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// Resizable html-panel
-$(function() {
-    $('#html-panel').resizable();
-});
+// Change html of output-panel iframe
+function updateOutput() {
+    $("#output-panel").contents().find("html").html("<html><head><style type='text/css'>" +
+        $('#css-panel').val() + "</style></head><body>" + $('#html-panel').val() +
+        "</body></html>");
+
+    document.getElementById('output-panel').contentWindow.eval($('#js-panel').val());
+}
+
+// Update the output-panel on load
+updateOutput();
 
 // Toggles colors on buttons when hovering
 $('.toggle-button').hover(function() {
@@ -12,14 +19,18 @@ $('.toggle-button').hover(function() {
 
 // When buttons are clicked, add/remove active class
 $('.toggle-button').click(function() {
-    $(this).toggleClass('active');
+    let panelId = $(this).attr('id') + '-panel';
+
+    $(this).toggleClass('active-btn');
+    $(this).removeClass('highlighted-button');
+    $('#' + panelId).toggleClass('active-panel');
+    $('#' + panelId).toggle();
 })
 
 // Sets the height of the panels
 $('#html-panel').height($('window').height() - $('#navbar').height());
 
-// Capture changes in html-panel
+// Capture changes in panels
 $('textarea').on('change keyup paste', function() {
-    // Change html of output-panel iframe
-    $("#output-panel").contents().find("html").html($('#html-panel').val());
+    updateOutput();
 });
